a5b68c371d77ebd7b0dbc712c47f75ff910b844e
raglandconnor <raglandconnor@ufl.edu>
2024-07-27 14:22:48 -0400

    Match home page styling

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -1,28 +1,22 @@
 import React from 'react';
-import { Button } from './Button';
+import { Button } from '@/components/ui/button';
 import './HeroSection.css';
+import Link from 'next/link';
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
+    <div className="hero-container">
       <h1>Reticketer</h1>
       <p>The Ultimate Ticket Reselling Platform</p>
-      <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-        >
-          Buy Tickets
-        </Button>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          Sell Tickets
-        </Button>
+      <div className="mt-8 scale-125">
+        <div>
+          <Link href="/search" className="mr-2">
+            <Button variant="outline">Search for Tickets</Button>
+          </Link>
+          <Link href="/sell-tickets">
+            <Button variant="outline">Sell Tickets</Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
---
+++ app/sell-tickets/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SellTickets = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center py-32">
+      sell tickets page
+    </div>
+  );
+};
+
+export default SellTickets;
