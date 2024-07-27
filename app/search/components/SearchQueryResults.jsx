1e8a9a7a84412c628a52bd39c24d4c9b1156e9c7
raglandconnor <raglandconnor@ufl.edu>
2024-07-27 19:37:05 -0400

    Add search query results to the search page using dummy data, add undo search button

--- app/search/components/SearchQueryResults.jsx
+++ app/search/components/SearchQueryResults.jsx
@@ -1,69 +1,70 @@
 'use client';
 import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 import axios from 'axios';
+import dummyData from '@/app/api/items/dummy_data';
+import { EventListingCard } from './EventListingCard';
+import { Button } from '@/components/ui/button';
 
 let displayResults = false;
-const SearchQueryResults = () => {
+
+const SearchQueryResults = ({ setListingsElements, setIsSearching }) => {
   const [eventName, setEventName] = useState('');
   const [locationName, setLocationName] = useState('');
   const [date, setDate] = useState('');
   const [filteredTickets, setFilteredTickets] = useState([]);
 
-  const [tickets, setTickets] = useState([]);
+  const [tickets, setTickets] = useState(dummyData);
 
-  const fetchTickets = async () => {
-    try {
-      const response = await axios.get('http://localhost:3001/getTickets');
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error fetching tickets:', error);
-    }
-  };
-
-  // useEffect(()=>{
-  //   axios.get('http:localhost:3001/getTickets')
-  //   .then(tickets=> setTickets(tickets.data))
-  //   .catch(err => console.log(err))
-  // })
   const eventChange = (event) => {
     setEventName(event.target.value);
-    console.log('here is the input ', event.target.value);
+    console.log('Event name input: ', event.target.value);
   };
+
   const locationChange = (event) => {
     setLocationName(event.target.value);
   };
+
   const dateChange = (event) => {
     setDate(event.target.value);
   };
 
-  const click = () => {
+  const onSearchClick = () => {
+    setIsSearching(false);
+    setIsSearching(true);
+
     const query = {};
     displayResults = false;
-    if (eventName) query.event = eventName.trim();
-    console.log(query.event, 'query event ');
-    if (locationName) query.location = locationName;
-    console.log(query.location, 'query location ');
-    if (date) query.date = date;
+    if (eventName) query.title = eventName.trim();
+    if (locationName) query.location = locationName.trim();
+    if (date) query.date = date.trim();
 
-    fetchTickets();
     if (Object.keys(query).length === 0) {
       setFilteredTickets(tickets);
     } else {
       const newFilteredTickets = tickets.filter((ticket) => {
         let isMatch = true;
-        for (const key in query) {
-          if (
-            query[key] &&
-            ticket[key] &&
-            !ticket[key]
-              .toString()
-              .toLowerCase()
-              .includes(query[key].toLowerCase())
-          ) {
-            isMatch = false;
-            break;
-          }
+
+        if (
+          query.title &&
+          !ticket.title.toLowerCase().includes(query.title.toLowerCase())
+        ) {
+          isMatch = false;
         }
+
+        if (
+          query.location &&
+          !`${ticket.location.city}, ${ticket.location.state}`
+            .toLowerCase()
+            .includes(query.location.toLowerCase())
+        ) {
+          isMatch = false;
+        }
+
+        if (query.date && ticket.date !== query.date) {
+          isMatch = false;
+        }
+
         return isMatch;
       });
 
@@ -71,48 +72,89 @@ const SearchQueryResults = () => {
     }
 
     displayResults = true;
+
+    setEventName('');
+    setLocationName('');
+    setDate('');
   };
 
-  const [searchQuery, setSearchQuery] = useState('temp search query');
-  const [numResults, setNumResults] = useState(93786);
+  const onUndoSearchClick = () => {
+    setIsSearching(false);
+  };
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    });
+  };
+
+  useEffect(() => {
+    const listingsElements = filteredTickets.map((listing) => {
+      const locationString = `${listing.location.city}, ${listing.location.state}`;
+
+      return (
+        <Link href={`listings/${listing.id}`} key={listing.id}>
+          <EventListingCard
+            seller={listing.name}
+            title={listing.title}
+            date={formatDate(listing.date)}
+            location={locationString}
+            price={listing.money}
+          />
+        </Link>
+      );
+    });
+
+    setListingsElements(listingsElements);
+  }, [filteredTickets]);
 
   return (
     <div className="flex flex-col items-center">
-      <div className="grid md:grid-cols-7 gap-4 items-center">
+      <div className="grid md:grid-cols-3 gap-4 items-center">
         <input
           onChange={eventChange}
           value={eventName}
-          className="md:col-span-2 border-2 border-black rounded p-2"
+          className="border-2 border-gray-300 rounded p-2"
           placeholder="Enter event name"
         />
         <input
           onChange={locationChange}
           value={locationName}
-          className="md:col-span-2 border-2 border-black rounded p-2"
+          className="border-2 border-gray-300 rounded p-2"
           placeholder="Enter location"
         />
         <input
           onChange={dateChange}
           value={date}
-          className="md:col-span-2 border-2 border-black rounded p-2"
+          className="border-2 border-gray-300 rounded p-2"
           placeholder="Enter date (YYYY-MM-DD)"
         />
+      </div>
+
+      <div className="grid grid-cols-2 gap-4 items-center mt-4">
         <button
-          onClick={click}
-          className="border-2 border-black bg-gray-100 rounded p-2"
+          onClick={onSearchClick}
+          className="border-2 text-white bg-black rounded-xl p-2 px-4 opacity-35 hover:opacity-40"
         >
           Search
         </button>
+        <button
+          onClick={onUndoSearchClick}
+          className="border-2 text-white bg-black rounded-xl p-2 px-4 opacity-35 hover:opacity-40"
+        >
+          Undo Search
+        </button>
       </div>
-
-      {displayResults && (
-        <div className="mt-4">
-          <p className="font-bold text-xl">{searchQuery}</p>
-          <p className="text-muted-foreground text-sm">
-            ({numResults} results)
-          </p>
-        </div>
-      )}
+      {/* {displayResults && (
+        <section>
+          <div className="p-6 grid md:grid-cols-2 2xl:grid-cols-3 gap-3">
+            {listingsElements}
+          </div>
+        </section>
+      )} */}
     </div>
   );
 };
--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,11 +1,27 @@
 'use client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchQueryResults from './components/SearchQueryResults';
 import { EventListingCard } from './components/EventListingCard';
 import dummyData from '../api/items/dummy_data.js';
 import Link from 'next/link';
+import { LoaderPinwheel } from 'lucide-react';
 
 const Search = () => {
+  const [listingsElements, setListingsElements] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSearching, setIsSearching] = useState(false);
+
+  useEffect(() => {
+    setGridElements();
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (!isSearching) {
+      setGridElements();
+    }
+  }, [isSearching]);
+
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     return date.toLocaleDateString('en-US', {
@@ -15,33 +31,47 @@ const Search = () => {
     });
   };
 
-  const listingsElements = dummyData.map((listing, index) => {
-    const locationString = `${listing.location.city}, ${listing.location.state}`;
+  const setGridElements = () => {
+    const listingsElements = dummyData.map((listing, index) => {
+      const locationString = `${listing.location.city}, ${listing.location.state}`;
 
-    return (
-      <Link href={`listings/${listing.id}`}>
-        <EventListingCard
-          key={listing.id}
-          seller={listing.name}
-          title={listing.title}
-          date={formatDate(listing.date)}
-          location={locationString}
-          price={listing.money}
-        />
-      </Link>
-    );
-  });
+      return (
+        <Link href={`listings/${listing.id}`}>
+          <EventListingCard
+            key={listing.id}
+            seller={listing.name}
+            title={listing.title}
+            date={formatDate(listing.date)}
+            location={locationString}
+            price={listing.money}
+          />
+        </Link>
+      );
+    });
+
+    setListingsElements(listingsElements);
+  };
 
   return (
     <div className="flex min-h-screen flex-col items-center py-32">
       <main className="md:w-[720px] lg:w-[860px] xl:w-[920px] 2xl:w-[1200px]">
         <section className="w-full border-b-2 pb-8">
-          <SearchQueryResults />
+          <SearchQueryResults
+            setListingsElements={setListingsElements}
+            setIsSearching={setIsSearching}
+          />
         </section>
         <section>
-          <div className="p-6 grid md:grid-cols-2 2xl:grid-cols-3 gap-3">
-            {listingsElements}
-          </div>
+          {isLoading && (
+            <div className="flex items-center justify-center mt-64 text-muted-foreground">
+              Loading...
+            </div>
+          )}
+          {!isLoading && (
+            <div className="p-6 grid md:grid-cols-2 2xl:grid-cols-3 gap-3">
+              {listingsElements}
+            </div>
+          )}
         </section>
       </main>
     </div>
