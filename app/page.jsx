fd51a9b642c96f7460171fe9eefb3b57f17edc92
raglandconnor <raglandconnor@ufl.edu>
2024-07-27 00:04:09 -0400

    Clear default components

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center py-24">
+      <p>hi</p>
+    </main>
+  );
+}
